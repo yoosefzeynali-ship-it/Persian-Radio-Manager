0c0a88023cdf26417f53a9adae695effdb26ee0e
yoosef <yoosefzeynali@gmail.com>
2026-07-24 10:49:57 +0330

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,1 +1,11 @@
+@echo off
 
+echo Building PersianRadioManager...
+
+pyinstaller --clean --noconfirm --onefile --windowed --icon=assets\logo.ico --add-data "assets;assets" --name PersianRadioManager src\main.py
+
+echo.
+
+echo Build Finished.
+
+pause
